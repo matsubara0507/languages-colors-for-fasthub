51fa6eea672a63a85736118f6ed9993e61292aa7
松原信忠 <t12307043@gunma-u.ac.jp>
2017-08-15 12:08:32 +0900

    first

---
+++ README.md
@@ -0,0 +1,11 @@
+# languages-colors-for-fasthub
+
+Check diff language colors on [GitHub](https://github.com/github/linguist/blob/master/lib/linguist/languages.yml) and [Haskell](https://github.com/k0shk0sh/FastHub/blob/master/app/src/main/assets/colors.json).
+
+This command is made in Haskell with stack.
+
+```
+$ stack exec -- check assets/languages.yml assets/colors.json
+("Fantom",(type @= Just "programming" <: aliases @= Nothing <: ace_mode @= Just "text" <: codemirror_mode @= Nothing <: wrap @= Nothing <: extensions @= Just [".fan"] <: interpreters @= Nothing <: searchable @= Nothing <: language_id @= 110 <: color @= Just "#14253c" <: tm_scope @= Just "source.fan" <: group @= Nothing <: nil,color @= Just "#dbded5" <: url @= "https://github.com/trending?l=Fantom" <: nil))
+("Haskell",(type @= Just "programming" <: aliases @= Nothing <: ace_mode @= Just "haskell" <: codemirror_mode @= Just "haskell" <: wrap @= Nothing <: extensions @= Just [".hs",".hsc"] <: interpreters @= Just ["runhaskell"] <: searchable @= Nothing <: language_id @= 157 <: color @= Just "#5e5086" <: tm_scope @= Nothing <: group @= Nothing <: nil,color @= Just "#29b544" <: url @= "https://github.com/trending?l=Haskell" <: nil))
+```
---
+++ src/Main.hs
@@ -0,0 +1,82 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedLabels      #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE PolyKinds             #-}
+{-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Main where
+
+import           Control.Lens        hiding ((:>))
+import           Data.Aeson          hiding (KeyValue)
+import           Data.Extensible
+import           Data.HashMap.Strict as HM
+import           Data.Proxy
+import           Data.String         (fromString)
+import           Data.Text           (Text)
+import           Data.Yaml           as Y
+import           GHC.TypeLits        (KnownSymbol, symbolVal)
+import           System.Environment  (getArgs)
+
+type GitHubLanguage =
+  Record '[
+    "type" :> Maybe Text,
+    "aliases" :> Maybe [Text],
+    "ace_mode" :> Maybe Text,
+    "codemirror_mode" :> Maybe Text,
+    "wrap" :> Maybe Bool,
+    "extensions" :> Maybe [Text],
+    "interpreters" :> Maybe [Text],
+    "searchable" :> Maybe Bool,
+    "language_id" :> Int,
+    "color" :> Maybe Text,
+    "tm_scope" :> Maybe Text,
+    "group" :> Maybe Text
+  ]
+
+type GitHubLanguages = HM.HashMap Text GitHubLanguage
+
+type FastHubLanguage =
+  Record '[
+    "color" :> Maybe Text,
+    "url" :> Text
+  ]
+
+type FastHubLanguages = HM.HashMap Text FastHubLanguage
+
+main :: IO ()
+main = do
+  [gfp, ffp] <- getArgs
+  glangs <- Y.decodeEither . fromString <$> readFile gfp :: IO (Either String GitHubLanguages)
+  flangs <- eitherDecode .fromString <$> readFile ffp :: IO (Either String FastHubLanguages)
+  either print (mapM_ print . HM.toList) $ diff <$> glangs <*> flangs
+  -- print glangs
+
+diff :: GitHubLanguages -> FastHubLanguages -> HM.HashMap Text (GitHubLanguage, FastHubLanguage)
+diff glangs flangs = HM.filter (not . uncurry eqColor)
+                   . HM.mapMaybeWithKey (\k v -> (,) v <$> HM.lookup k flangs)
+                   $ glangs
+
+eqColor :: GitHubLanguage -> FastHubLanguage -> Bool
+eqColor glang flang = glang ^. #color == flang ^. #color
+
+instance Forall (KeyValue KnownSymbol FromJSON') xs => FromJSON (Record xs) where
+  parseJSON = withObject "Object" $
+    \v -> hgenerateFor (Proxy :: Proxy (KeyValue KnownSymbol FromJSON')) $
+    \m -> let k = symbolVal (proxyAssocKey m) in
+      case HM.lookup (fromString k) v of
+        Just a  -> Field . return <$> parseJSON a
+        Nothing ->
+          maybe (fail $ "Missing key: " `mappend` k) (fmap (Field . return)) $ parseJSON' Nothing
+
+class FromJSON a => FromJSON' a where
+  parseJSON' :: Maybe Value -> Maybe (Parser a)
+
+instance FromJSON a => FromJSON' a where
+  parseJSON' = fmap parseJSON
+
+instance {-# OVERLAPS #-} FromJSON a => FromJSON' (Maybe a) where
+  parseJSON' = Just . maybe (pure Nothing) parseJSON
